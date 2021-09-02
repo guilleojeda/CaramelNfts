bdb6dd4c5e7fa1355ddf3b13d2d4b9bb076e42a8
Guillermo <guillermo@caramelpoint.com>
2021-09-02 15:57:25 -0300

    updated contract

--- contracts/CaramelNfts.sol
+++ contracts/CaramelNfts.sol
@@ -2,13 +2,43 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CaramelNfts is ERC721PresetMinterPauserAutoId {
+contract CaramelNfts is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
 
-    constructor() ERC721PresetMinterPauserAutoId("CaramelToken", "CARAMEL", "https://www.caramelpoint.com/nfts/metadata/") {}
+    Counters.Counter private _tokenIdCounter;
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-       return string(abi.encodePacked(super.tokenURI(tokenId),".json"));
+    constructor() ERC721("CaramelToken", "CARAMEL") {}
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+    
+    function burnToken(uint256 tokenId) public onlyOwner {
+        _burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    
+    function mintNFT(address recipient, string memory jsonUri) public onlyOwner returns (uint256)
+    {
+        _safeMint(recipient, _tokenIdCounter.current());
+
+        uint256 newItemId = _tokenIdCounter.current();
+        super._setTokenURI(newItemId, jsonUri);
+        _tokenIdCounter.increment();
+        return newItemId;
     }
 }
